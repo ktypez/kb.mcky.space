ec971642294a5061bcae392504061d92a9cc7b19
ktypez <keitochan@gmail.com>
2026-07-14 01:08:12 +0000

    reset: restore default Starlight theme (remove starlight-theme-black plugin)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 import sidebar from './src/data/sidebar.json';
-import starlightThemeBlack from 'starlight-theme-black';
 
 export default defineConfig({
   output: 'static',
@@ -12,12 +11,6 @@ export default defineConfig({
       sidebar,
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/ktypez/OKF' },
-      ],
-      plugins: [
-        starlightThemeBlack({
-          footerText:
-            'Built & designed by [shadcn](https://twitter.com/shadcn). Ported to Astro Starlight by [Adrián UB](https://github.com/adrian-ub).',
-        }),
       ],
     }),
   ],
